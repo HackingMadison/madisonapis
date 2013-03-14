c9122fd55d77c708e9ef5d5233b4e421f3972303
Steve Faulkner <southpolesteve@gmail.com>
2013-03-13 21:07:35 -0500

    Move more logic into base api class

--- app/models/madison_api.rb
+++ app/models/madison_api.rb
@@ -13,26 +13,22 @@ class MadisonApi
 
   def self.import_from_source
     csv.each do |row|
-      park = Park.where(ward: row['Ward']).first_or_initialize
+      record = self.where(ward: row['Ward']).first_or_initialize
       row.to_hash.keys.each do |key|
         if !skip_fields.include?(key)
-          if park.respond_to?(key.parameterize.underscore+"=")
-            park.send(key.parameterize.underscore+"=",row.to_hash[key])
+          if record.respond_to?(key.parameterize.underscore+"=")
+            record.send(key.parameterize.underscore+"=",row.to_hash[key])
           else
-            park[key.parameterize.underscore] = convert_value(row.to_hash[key])
+            record[key.parameterize.underscore] = convert_value(row.to_hash[key])
           end
         end
       end
-      park.save!
+      record.save!
     end
   end
 
   def self.skip_fields
     []
-  end
-
-  def location=(string)
-    self.coordinates = [string.split(", ")[1].gsub(")","").to_f, string.split(", ")[0].gsub("(","").to_f]
   end
 
   def self.convert_value(value)
--- app/models/park.rb
+++ app/models/park.rb
@@ -13,22 +13,6 @@ class Park < MadisonApi
     'https://data.cityofmadison.com/api/views/svr6-6gvb/rows.csv?accessType=DOWNLOAD'
   end
 
-  def self.import_from_source
-    csv.each do |row|
-      park = Park.where(source_id: row['Id']).first_or_initialize
-      row.to_hash.keys.each do |key|
-        if !skip_fields.include?(key)
-          if park.respond_to?(key.parameterize.underscore+"=")
-            park.send(key.parameterize.underscore+"=",row.to_hash[key])
-          else
-            park[key.parameterize.underscore] = convert_value(row.to_hash[key])
-          end
-        end
-      end
-      park.save!
-    end
-  end
-
   def self.skip_fields
     ["Id"]
   end
