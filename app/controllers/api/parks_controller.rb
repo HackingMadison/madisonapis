f8dbe8906bfffe41252be0b0150b7dbf15e3c81b
Steve Faulkner <southpolesteve@gmail.com>
2013-03-14 23:38:51 -0500

    Blargh. Unbreak geo located parks

--- app/controllers/api/parks_controller.rb
+++ app/controllers/api/parks_controller.rb
@@ -2,7 +2,7 @@ class Api::ParksController < ApplicationController
 
   def index
     @records = Park.unscoped
-    @records = @records.near(params[:location].split(",").map(&:to_f), params[:distance] || 1) if params[:location]
+    @records = Park.near(params[:location].split(",").map(&:to_f), params[:distance] || 1) if params[:location]
     @records = @records.page(params[:page]).per(25)
     render json: @records, meta: { total_records: @records.total_count,
                                  current_page: @records.current_page,
